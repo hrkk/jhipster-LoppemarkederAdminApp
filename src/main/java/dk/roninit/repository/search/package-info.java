21670065eda17295555d83250dd44fb61f8c4fad
hrkk <kasper.odgaard@gmail.com>
2018-04-02 11:24:26 +0200

    initial commit

---
+++ src/main/java/dk/roninit/repository/search/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring Data Elasticsearch repositories.
+ */
+package dk.roninit.repository.search;
---
+++ src/test/java/dk/roninit/cucumber/stepdefs/StepDefs.java
@@ -0,0 +1,18 @@
+package dk.roninit.cucumber.stepdefs;
+
+import dk.roninit.LoppemarkederAdminApp;
+
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.ResultActions;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@WebAppConfiguration
+@SpringBootTest
+@ContextConfiguration(classes = LoppemarkederAdminApp.class)
+public abstract class StepDefs {
+
+    protected ResultActions actions;
+
+}
